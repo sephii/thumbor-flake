9edaffbad1a5c267d4d2788f51d31d48450d8551
Sylvain Fankhauser <sephi@nibelheim.ch>
2025-10-18 16:34:05 +0200

    Update NixOS module

--- module.nix
+++ module.nix
@@ -7,6 +7,8 @@
 
 let
   cfg = config.services.thumbor;
+
+  settingsFormat = pkgs.formats.pythonVars { };
 in
 {
 
@@ -39,18 +41,26 @@ in
       '';
     };
 
-    pythonConfig = mkOption {
-      type = types.str;
-      default = "";
+    settings = mkOption {
+      type = settingsFormat.type;
+      default = { };
       description = ''
-        The contents of the configuration file, as Python code.
+        Configuration for thumbor.
 
+        See <https://github.com/thumbor/thumbor/blob/master/thumbor/config.py> for available options.
+
+        ::: {.warning}
         Do not put your keyfile in here! Use the `keyFile` option.
+        :::
       '';
-      example = ''
-        import os
+      example = lib.literalExpression ''
+        let
+          format = pkgs.formats.pythonVars { };
+        in {
+          _includes = [ "os" ];
 
-        ACCESS_CONTROL_ALLOW_ORIGIN_HEADER = os.environ["THUMBOR_ACCESS_CONTROL_ALLOW_ORIGIN_HEADER"]
+          ACCESS_CONTROL_ALLOW_ORIGIN_HEADER = format.lib.mkRaw "os.environ[\"THUMBOR_ACCESS_CONTROL_ALLOW_ORIGIN_HEADER\"]";
+        }
       '';
     };
 
@@ -92,6 +102,17 @@ in
         Number of processes to start. Set to 0 to start a number of processes equal to the number of CPU cores.
       '';
     };
+
+    environmentFile = mkOption {
+      type = types.nullOr types.str;
+      default = null;
+      description = ''
+        File with environment variables to pass to thumbor, in the format used
+        by Systemd EnvironmentFile (`VAR=value` entries, separated by
+        newlines). Values in this file will override any configuration set in
+        `settings`.
+      '';
+    };
   };
 
   meta.maintainers = [ lib.maintainers.sephi ];
@@ -107,24 +128,41 @@ in
       wantedBy = [ "multi-user.target" ];
       serviceConfig =
         let
-          configFile = pkgs.writeText "thumbor_config.py" cfg.pythonConfig;
+          configFile = settingsFormat.generate "thumbor_config.py" cfg.settings;
 
-          startOptions = [
-            "-c ${configFile}"
-            "-l ${cfg.logLevel}"
-            "-i ${cfg.listenAddress}"
-            "-p ${toString cfg.port}"
-            "--processes ${toString cfg.processes}"
-          ]
-          ++ (lib.optional (cfg.fileDescriptor != null) "-f ${cfg.fileDescriptor}")
-          ++ (lib.optional (cfg.keyFile != null) "-k ${cfg.keyFile}")
-          ++ (lib.optional cfg.debug "-d");
+          startOptions = lib.cli.toGNUCommandLineShell { } {
+            c = configFile;
+            l = cfg.logLevel;
+            i = cfg.listenAddress;
+            p = cfg.port;
+            f = cfg.fileDescriptor;
+            k = cfg.keyFile;
+            d = cfg.debug;
+            processes = cfg.processes;
+          };
         in
         {
-          ExecStart = ''${lib.getExe cfg.package} ${lib.concatStringsSep " " startOptions}'';
+          ExecStart = ''${lib.getExe cfg.package} --use-environment 1 ${startOptions}'';
+          EnvironmentFile = lib.optional (cfg.environmentFile != null) cfg.environmentFile;
           User = "thumbor";
           Group = "thumbor";
-          PrivateTmp = "true";
+          LockPersonality = true;
+          MemoryDenyWriteExecute = true;
+          NoNewPrivileges = true;
+          PrivateDevices = true;
+          PrivateTmp = true;
+          ProtectClock = true;
+          ProtectControlGroups = true;
+          ProtectHome = true;
+          ProtectHostname = true;
+          ProtectKernelModules = true;
+          ProtectKernelLogs = true;
+          ProtectKernelTunables = true;
+          ProtectSystem = "full";
+          RestrictNamespaces = true;
+          RestrictRealtime = true;
+          RestrictSUIDSGID = true;
+          RemoveIPC = true;
         };
     };
   });
